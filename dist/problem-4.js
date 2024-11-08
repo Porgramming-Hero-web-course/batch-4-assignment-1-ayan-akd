938879426c4b607974a6ee4cc68fb087321451b9
ayan-akd <ayankumar.akd@gmail.com>
2024-11-08 08:49:40 +0600

    solved 4 problems

--- dist/problem-4.js
+++ dist/problem-4.js
@@ -1,4 +1,21 @@
 "use strict";
 {
-    // Define a union type Circle and Rectangle, where each type has a unique shape property. Create a function calculateShapeArea that uses type guards to calculate the area based on the shape type.
+    const calculateShapeArea = (shape) => {
+        if (shape.shape === "circle") {
+            const circleArea = parseFloat((Math.PI * shape.radius * shape.radius).toFixed(2));
+            return circleArea;
+        }
+        else {
+            const rectangleArea = shape.width * shape.height;
+            return rectangleArea;
+        }
+    };
+    const circleArea = calculateShapeArea({ shape: "circle", radius: 5 });
+    console.log(circleArea);
+    const rectangleArea = calculateShapeArea({
+        shape: "rectangle",
+        width: 4,
+        height: 7,
+    });
+    console.log(rectangleArea);
 }
--- src/problem-4.ts
+++ src/problem-4.ts
@@ -1,14 +1,36 @@
 {
-   // Define a union type Circle and Rectangle, where each type has a unique shape property. Create a function calculateShapeArea that uses type guards to calculate the area based on the shape type.
+  // Define a union type Circle and Rectangle, where each type has a unique shape property. Create a function calculateShapeArea that uses type guards to calculate the area based on the shape type.
 
+  type Circle = {
+    shape: "circle";
+    radius: number;
+  };
 
+  type Rectangle = {
+    shape: "rectangle";
+    width: number;
+    height: number;
+  };
 
+  type shape = Circle | Rectangle;
 
-
-
-
-
-
-
-
+  const calculateShapeArea = (shape: shape): number => {
+    if (shape.shape === "circle") {
+      const circleArea = parseFloat(
+        (Math.PI * shape.radius * shape.radius).toFixed(2)
+      );
+      return circleArea;
+    } else {
+      const rectangleArea = shape.width * shape.height;
+      return rectangleArea;
+    }
+  };
+  const circleArea = calculateShapeArea({ shape: "circle", radius: 5 });
+  console.log(circleArea);
+  const rectangleArea = calculateShapeArea({
+    shape: "rectangle",
+    width: 4,
+    height: 7,
+  });
+  console.log(rectangleArea);
 }
